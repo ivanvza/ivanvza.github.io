31852c0127bf997784b5d5e2e2f33f9aa7f22878
Ivan Viljoen <8543825+ivanvza@users.noreply.github.com>
2026-01-02 23:05:32 +0400

    removing analytics

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Ivanvza Blog",
+    pageTitle: "0xIvan",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
